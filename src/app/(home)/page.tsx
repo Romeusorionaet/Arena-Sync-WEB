2bf99c8979cda088828b44d75f471460aa439f4f
Romeu Sorionaet <romeuindexjs@gmail.com>
2024-10-27 11:13:13 -0300

    first commit: created project, created first section molde in home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="">
+      <section className="flex h-[40rem] max-md:flex-col">
+        <div className="h-full w-full bg-black md:w-1/2">1</div>
+
+        <div className="h-full w-full border border-black md:w-1/2">2</div>
+      </section>
+    </main>
+  )
+}
