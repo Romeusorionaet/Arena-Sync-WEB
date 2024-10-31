a10f00e3c1f2580263e2910ca34088e978168413
Romeu Sorionaet <romeuindexjs@gmail.com>
2024-10-31 12:03:04 -0300

    feat: created skeleton and error components

---
+++ src/app/components/skeletons/upcoming-matches-skeleton.tsx
@@ -0,0 +1,22 @@
+export function UpcomingMatchesSkeleton() {
+  return (
+    <div className="flex w-80 flex-col gap-2 md:w-[25rem]">
+      <div className="flex w-full gap-2">
+        <div className="h-14 w-full animate-pulse rounded-l-lg bg-zinc-300"></div>
+        <div className="h-14 w-24 animate-pulse rounded-e-lg bg-zinc-300"></div>
+      </div>
+      <div className="flex w-full gap-2">
+        <div className="h-14 w-full animate-pulse rounded-l-lg bg-zinc-300"></div>
+        <div className="h-14 w-24 animate-pulse rounded-e-lg bg-zinc-300"></div>
+      </div>
+      <div className="flex w-full gap-2">
+        <div className="h-14 w-full animate-pulse rounded-l-lg bg-zinc-300"></div>
+        <div className="h-14 w-24 animate-pulse rounded-e-lg bg-zinc-300"></div>
+      </div>
+      <div className="flex w-full gap-2">
+        <div className="h-14 w-full animate-pulse rounded-l-lg bg-zinc-300"></div>
+        <div className="h-14 w-24 animate-pulse rounded-e-lg bg-zinc-300"></div>
+      </div>
+    </div>
+  )
+}
