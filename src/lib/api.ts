23ea37288bc987d8d36e0bbb6cf01096a7cf8bc1
Romeu Sorionaet <romeuindexjs@gmail.com>
2024-10-31 23:24:50 -0300

    refactor: removed headers

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,4 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
 })
